ec370c50906c4d22b88a3dc35e2ccabec0d10bc1
anmorales-hub <moralesand2018@gmail.com>
2020-04-02 22:29:46 -0400

    Task: Add SQL database screenshot

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ This project is a homework assignment to teach how to get Pycharm setup with Doc
 
 #Postman Screenshot
 #SQL Screenshot
+![pycharm data query](screenshots/database result1.jpg)
 
 # Readings
 * [What is an HTTP API?](https://www.smashingmagazine.com/2018/01/understanding-using-rest-api/)
--- app/app.py
+++ app/app.py
@@ -27,16 +27,12 @@ def cities_import() -> List[Dict]:
     return result
 
 
-
-
-
 @app.route('/')
 
 def index() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
